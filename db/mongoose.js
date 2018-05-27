aa04563662424f7873c40413c9f2f995ef7e7558
Aman Gupta <39579933+aman1695@users.noreply.github.com>
2018-05-28 02:12:43 +0530

    removed mongodb credentials

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MovieDB');
-mongoose.connect('mongodb://admin:admin@ds135810.mlab.com:35810/aman-moviedb');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/MovieDB');
 module.exports = {mongoose};
